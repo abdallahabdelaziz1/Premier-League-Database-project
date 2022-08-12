159fc297b4a003d900efbe5752e7d7fb4a41d685
Abdallah Abdelaziz <abdallah_taha@aucegypt.edu>
2022-08-12 20:04:50 +0200

    feat: Add database schema file
    Adding database schema file that shows SQL transcript for creating
    database.

---
+++ PremierLeague.sql
@@ -0,0 +1,96 @@
+CREATE SCHEMA IF NOT EXISTS `premierleague`;
+
+CREATE TABLE IF NOT EXISTS Player (
+	PlayerName VARCHAR(30) NOT NULL PRIMARY KEY,
+    BirthDate DATE NOT NULL,
+    Weight INT,
+    Height INT NOT NULL,
+    `Position` VARCHAR(20) NOT NULL,
+    Nationality VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Stadium (
+	StadiumName VARCHAR(50) NOT NULL PRIMARY KEY,
+    RecordAttendence INT NOT NULL,
+    DateBuiling YEAR NOT NULL,
+    Capacity INT NOT NULL,
+    PitchWidth INT NOT NULL,
+    PitchLength INT NOT NULL,
+    AddressCity VARCHAR(20) NOT NULL,
+    AddressPC VARCHAR(10) NOT NULL,
+    AddressArea VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Club (
+	ClubName VARCHAR(30) NOT NULL PRIMARY KEY,
+    Website VARCHAR(70) NOT NULL,
+    ClubStadium VARCHAR(50) NOT NULL,
+    CONSTRAINT FK_Owns FOREIGN KEY Club(ClubStadium) REFERENCES Stadium(StadiumName)
+    ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS PlaysFor (
+	PlayerName VARCHAR(30) NOT NULL,
+    ClubName VARCHAR(30) NOT NULL,
+    Season CHAR(7) NOT NULL,
+    PRIMARY KEY(PlayerName, ClubName, Season),
+    CONSTRAINT FK_PlaysForPlayer FOREIGN KEY PlaysFor(PlayerName) REFERENCES Player(PlayerName)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT FK_PlaysForClub FOREIGN KEY PlaysFor(ClubName) REFERENCES Club(ClubName)
+    ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS `Match` (
+    HomeClub VARCHAR(30) NOT NULL,
+    AwayClub VARCHAR(30) NOT NULL,
+    Season CHAR(7) NOT NULL,
+    MatchDate Date NOT NULL,
+    HomeFouls INT NOT NULL,
+    HomeGoals INT NOT NULL,
+    HomeYellow INT NOT NULL,
+    HomeRed INT NOT NULL,
+    HomeShots INT NOT NULL,
+    HomePossession DECIMAL(4,2) NOT NULL,
+    AwayFouls INT NOT NULL,
+    AwayGoals INT NOT NULL,
+    AwayYellow INT NOT NULL,
+    AwayRed INT NOT NULL,
+    AwayShots INT NOT NULL,
+    AwayPossession DECIMAL(4,2) NOT NULL,
+    StadiumName VARCHAR(50) NOT NULL,
+    PRIMARY KEY(HomeClub, AwayClub, Season),
+    CONSTRAINT FK_MatchHome FOREIGN KEY `Match`(HomeClub) REFERENCES Club(ClubName)
+    ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT FK_MatchAway FOREIGN KEY `Match`(AwayClub) REFERENCES Club(ClubName)
+    ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT FK_Playedin FOREIGN KEY `Match`(StadiumName) REFERENCES Stadium(StadiumName)
+    ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS Fan (
+	Email VARCHAR(100) NOT NULL PRIMARY KEY,
+    `Password` CHAR(32) NOT NULL,
+    UserName VARCHAR(20) NOT NULL,
+    Gender CHAR NOT NULL,
+    FavClub VARCHAR(30) NOT NULL,
+    BirthDate DATE NOT NULL,
+    CONSTRAINT FK_Favorite FOREIGN KEY Fan(FavClub) REFERENCES Club(ClubName)
+    ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS MatchReview (
+	FanEmail VARCHAR(100) NOT NULL,
+    HomeClub VARCHAR(30) NOT NULL,
+    AwayClub VARCHAR(30) NOT NULL,
+    Season CHAR(7) NOT NULL,
+    Rating INT NOT NULL,
+    ReviewText VARCHAR(1000) NOT NULL,
+    CONSTRAINT Rating_Check CHECK(Rating BETWEEN 1 AND 10),
+    PRIMARY KEY(FanEmail, HomeClub, AwayClub, Season),
+    CONSTRAINT FK_MatchReviwed FOREIGN KEY MatchReview(HomeClub, AwayClub, Season) REFERENCES `Match`(HomeClub, AwayClub, Season)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT FK_Reviews FOREIGN KEY MatchReview(FanEmail) REFERENCES Fan(Email)
+    ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
